70d77662f022b6e52a71507718a243809773d7da
souhaibouadi <147671244+souhaibouadi@users.noreply.github.com>
2026-05-13 01:30:13 +0100

    add dental_clinic/models/__init__.py

---
+++ dental_clinic/models/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from . import dental_patient
+from . import dental_medical_history
+from . import dental_practitioner
+from . import dental_room
+from . import dental_tooth
+from . import dental_treatment
+from . import dental_treatment_plan
+from . import dental_treatment_plan_line
+from . import dental_appointment
+from . import dental_prescription
+from . import dental_prescription_line
+from . import res_partner
+from . import account_move
